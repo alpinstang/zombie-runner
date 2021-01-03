1cd4447ad9f86a5a5093168f65eccf22b767aaec
John McDonald <jmcdonald1991@gmail.com>
2021-01-03 00:36:27 -0600

    rotate and muzzle flash

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -8,16 +8,53 @@ public class Weapon : MonoBehaviour
     [SerializeField] Camera FPCamera;
     [SerializeField] float range = 100f;
     [SerializeField] float damage = 30f;
+    [SerializeField] GameObject muzzleFlash;
+
+    private void Start()
+    {
+        muzzleFlash.SetActive(false);
+    }
 
     void Update()
     {
         if (Input.GetButtonDown("Fire1"))
         {
             Shoot();
+        } else if(Input.GetButtonUp("Fire1"))
+        {
+            muzzleFlash.SetActive(false);
         }
+
     }
 
     private void Shoot()
+    {
+        PlayMuzzleFlash();
+        ProcessRaycast();
+    }
+
+    private void PlayMuzzleFlash()
+    {
+        StartCoroutine(Rotate(0.1f));
+        muzzleFlash.SetActive(true);
+    }
+
+    IEnumerator Rotate(float duration)
+    {
+        float startRotation = muzzleFlash.transform.eulerAngles.z;
+        float endRotation = startRotation + 360.0f;
+        float t = 0.0f;
+        while (t < duration)
+        {
+            t += Time.deltaTime;
+            float zRotation = Mathf.Lerp(startRotation, endRotation, t / duration) % 360.0f;
+            muzzleFlash.transform.eulerAngles = new Vector3(muzzleFlash.transform.eulerAngles.x, muzzleFlash.transform.eulerAngles.y,
+            zRotation);
+            yield return null;
+        }
+    }
+
+    private void ProcessRaycast()
     {
         RaycastHit hit;
         Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range);

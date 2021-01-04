c772da8194b21ba0879ecca3d090db3be4bce78f
John McDonald <jmcdonald1991@gmail.com>
2021-01-03 18:57:07 -0600

    hit effect particles

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -9,6 +10,7 @@ public class Weapon : MonoBehaviour
     [SerializeField] float range = 100f;
     [SerializeField] float damage = 30f;
     [SerializeField] GameObject muzzleFlash;
+    [SerializeField] GameObject hitEffect;
 
     private void Start()
     {
@@ -35,7 +37,7 @@ public class Weapon : MonoBehaviour
 
     private void PlayMuzzleFlash()
     {
-        StartCoroutine(Rotate(0.1f));
+        // StartCoroutine(Rotate(0.1f));
         muzzleFlash.SetActive(true);
     }
 
@@ -57,12 +59,9 @@ public class Weapon : MonoBehaviour
     private void ProcessRaycast()
     {
         RaycastHit hit;
-        Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range);
-        Debug.Log("I hit this thing: " + hit.transform.name);
         if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
         {
-            Debug.Log("I hit this thing: " + hit.transform.name);
-            // TODO: add some hit effect for visual players
+            CreateHitImpact(hit);
             EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
             if (target == null) return;
             target.TakeDamage(damage);
@@ -71,5 +70,11 @@ public class Weapon : MonoBehaviour
         {
             return;
         }
+    }
+
+    private void CreateHitImpact(RaycastHit hit)
+    {
+        GameObject impact = Instantiate(hitEffect, hit.point, Quaternion.LookRotation(hit.normal));
+        Destroy(impact, 1);
     }
 }
